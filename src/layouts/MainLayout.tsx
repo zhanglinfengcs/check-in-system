ee1c4e8dde5f1b809bb536e495209e6c349c6786
Zhang Linfeng <zhanglinfengcs@outlook.com>
2023-04-26 11:42:39 +0800

    feat: Add auth guard

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import SideNav from "./SideNav.tsx";
+import { UserType, UserLevel } from "../types/index.tsx";
+import useUser from "../hooks/useUser.tsx"
+
+type SideNavItemType = {
+  title: string;
+  path: string;
+  icon?: React.ReactNode;
+  current: boolean;
+};
+
+const simpleSideNavItems: SideNavItemType[] = [
+  { title: "Home", path: "/home", current: false },
+  { title: "Posts", path: "/posts", current: false },
+  { title: "Leave", path: "/leave", current: false },
+  { title: "Account", path: "/account", current: false },
+];
+
+const adminSideNavItems: SideNavItemType[] = [
+  { title: "Home", path: "/home", current: false },
+]
 
 const MainLayout: React.FC = () => {
+  const { user } = useUser()
+  const { level } = user as UserType
 
   return (
     <div className="w-screen h-screen grid xl:grid-cols-[1fr_4fr] 2xl:grid-cols-[1fr_6fr]">
       <nav>
-        <SideNav />
+        {
+          level === UserLevel.Admin ? <SideNav props={adminSideNavItems}/> : <SideNav props={simpleSideNavItems} />
+        }  
       </nav>
       <main className=" my-3 mr-3 rounded-lg overflow-y-auto bg-gray-100">
         <Outlet />
